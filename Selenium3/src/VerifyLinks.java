3100e20378aa7fb1a6f3076e5db790b8bf1ec0a3
rahul.kulkarni <rahul.kulkarni@yahoo.com>
2016-11-18 16:49:11 +0000

    Added VerifyURL existence

---
+++ Selenium3/src/VerifyLinks.java
@@ -0,0 +1,61 @@
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import Helper.BrowserFactory;
+
+/**
+ * 
+ */
+
+/**
+ * @author rahul.kulkarni
+ *
+ */
+public class VerifyLinks {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		WebDriver driver = BrowserFactory.startBrowser("chrome", "https://www.google.com");
+		List<WebElement> links = driver.findElements(By.tagName("a"));
+		
+		System.out.println("Total Links on this page are = " + links.size());
+		
+		
+		for (int i =0;i<links.size();i++){
+			WebElement ele = links.get(i);
+			String url = ele.getAttribute("href");
+			verifyLinkIsActive(url);
+		}
+		
+		
+	}
+
+	private static void verifyLinkIsActive(String urlLink) {
+		try{
+				URL url = new URL(urlLink);
+				HttpURLConnection httoURLConnect = (HttpURLConnection)url.openConnection();
+				httoURLConnect.setConnectTimeout(3000);
+				
+				httoURLConnect.connect();
+				if (httoURLConnect.getResponseCode()==200){
+					System.out.println(urlLink + "-"+httoURLConnect.getResponseMessage());
+				}
+				if (httoURLConnect.getResponseCode()==HttpURLConnection.HTTP_NOT_FOUND){
+					System.out.println(urlLink + " - "	+httoURLConnect.getResponseMessage() + " - " 
+														+HttpURLConnection.HTTP_NOT_FOUND);
+					}
+			}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+}
--- Selenium3/src/mytest/FindHiddenWebElement.java
+++ Selenium3/src/mytest/FindHiddenWebElement.java
@@ -3,6 +3,8 @@ package mytest;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 import Helper.BrowserFactory;
 
@@ -24,7 +26,36 @@ public class FindHiddenWebElement {
 		    driver.switchTo().window(winHandle); // switch focus of WebDriver to the next found window handle (that's your newly opened window)
 		}
 		
-		driver.findElement(By.cssSelector("em[class='x-btn-split'][style='background-color: transparent;']")).click();
+		//driver.findElement(By.cssSelector("em[class='x-btn-split']")).click();
+
+		//driver.findElement(By.cssSelector("em[id^=ext-gen]")).click();
+		System.out.println("Sleeping1");
+		Thread.sleep(1000);
+		driver.findElement(By.cssSelector("em.x-btn-split")).click();
+		System.out.println("Sleeping2");
+		Thread.sleep(1000);
+		driver.findElement(By.cssSelector("em.x-btn-split")).click();
+		System.out.println("Sleeping3");
+		Thread.sleep(1000);
+		driver.findElement(By.cssSelector("em.x-btn-split")).click();
+		
+		Actions action = new Actions(driver);
+		
+		action.doubleClick().build().perform();
+		
+		
+		/*WebElement list = driver.findElement(By.xpath("html/body/div[4]/div[1]/div[1]/table/tbody/tr[2]/td[2]//*[@class='x-btn-split']"));
+		//list.click();
+		Actions act = new Actions(driver);
+			act.doubleClick(list);
+			
+		
+		System.out.println("Sleeping");
+		Thread.sleep(5000);
+		WebElement cust = list.findElement(By.linkText("Customers"));
+		Thread.sleep(1000);
+		cust.click();*/
+		
 		System.out.println("Done");
 		//driver.close(); 
 		driver.switchTo().window(parentHandle); 
